430b4ae7b27396195c46d20ab89462532e4f823e
mvinoth15 <indianvkumar@gmail.com>
2021-04-15 12:12:34 +0530

    GitSample

---
+++ src/test/java/org/gitsample/SampleGit.java
@@ -0,0 +1,16 @@
+package org.gitsample;
+
+public class SampleGit {
+	public static  void main(String []args) {
+		int a=100;
+		for(int i=0;i<=a;i++) {
+			if(i%2==0)
+			{
+				
+				System.out.println(i);
+			}
+			
+		}
+			}
+
+}
